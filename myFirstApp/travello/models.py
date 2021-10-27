37563e1efd4e282327b0c30c9f2c2d0d5100124b
Shouhardik Saha <60508616+shouhardik@users.noreply.github.com>
2021-10-27 12:39:12 +0530

    Updated the chances

--- myFirstApp/travello/models.py
+++ myFirstApp/travello/models.py
@@ -1,6 +1,11 @@
 from django.db import models
-
+from django.utils.timezone import now
+import calendar
 # Create your models here.
+
+NEWS_CHOICE=[   #PLEASE ADD ACCORDING TO YOUR APPLICATION . I HAVE AN EXAMPLE.
+'Sports','Entertaintment','Movies'
+]
 
 class Destination(models.Model) :
     name = models.CharField(max_length = 100)
@@ -9,11 +14,11 @@ class Destination(models.Model) :
     price = models.IntegerField()
     offer = models.BooleanField(default = False)
         
-class News() :
-    id : int
-    img : str
-    date : int
-    month : str
-    headline : str
-    category : str
-    desc : str
+class News(models.Model) :
+    id = models.IntegerField(default=1)
+    img = models.ImageField(uploaded_to='pics')
+    date = models.DateTimeField(default=now,editable=False)
+   
+    headline = models.CharField(max_length = 200, default=News Today)# DEFAULT HEADLINE News Today AN EXAMPLE
+    category = models.CharField(max_length = 200,choices=NEWS_CHOICE)
+    desc =  models.TextField()
